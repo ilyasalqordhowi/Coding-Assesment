cc4519d9b477b2388ac72d0ea039ae4bc3f3f666
ilyas <ilyasalqordhowi@gmail.com>
2024-10-23 21:34:50 +0700

    fix backend

--- backend/migrations/000003_create_task.up.sql
+++ backend/migrations/000003_create_task.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE "task"(
     "id" SERIAL PRIMARY KEY,
     "title" VARCHAR(50),
     "descriptions" TEXT,
+    "date" VARCHAR(255),
     "statusId" INT REFERENCES "status_task"("id")
 );
